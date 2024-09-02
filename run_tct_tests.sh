9c7251ca5574b17f172cfbc5bc0f4b6b28b4f2a3
Collins-Webdev <collinshaya@gmail.com>
2024-09-02 11:34:57 +0200

    test

---
+++ run_tct_tests.sh
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     1746       1    1746      20056  ?         197609 13:15:43 /usr/bin/mintty
+     1791    1747    1791      15552  pty0      197609 13:15:50 /usr/bin/PS
+     1747    1746    1747      25644  pty0      197609 13:15:43 /usr/bin/bash
